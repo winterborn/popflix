656af17580f47c82277d743cc4050a92addd9f0a
hakareem <harithakareem72@gmail.com>
2022-10-03 12:27:15 +0100

    project setup

---
+++ src/test/java/com/project/popflix/PopflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.popflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
