eb4d693676ec8672042a99aae38e50dabe67a7af
hakareem <harithakareem72@gmail.com>
2022-10-05 15:43:24 +0100

    movies controller

--- src/main/java/com/project/popflix/controller/MoviesController.java
+++ src/main/java/com/project/popflix/controller/MoviesController.java
@@ -3,6 +3,7 @@ package com.project.popflix.controller;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -30,64 +31,83 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Controller
 public class MoviesController {
 
- @Autowired
- UserRepository userRepository;
- @Autowired
- AuthoritiesRepository authoritiesRepository;
+  @Autowired
+  UserRepository userRepository;
+  @Autowired
+  AuthoritiesRepository authoritiesRepository;
 
- private Long getUserId() {
-  SecurityContext context = SecurityContextHolder.getContext();
-  Authentication authentication = context.getAuthentication();
-  Long id = userRepository.findByUsername(authentication.getName()).getId();
-  return id;
- }
-
- private String getMovieVideoLink(int id) {
-  TmdbMovies movies = new TmdbApi("d84f9365179dc98dc69ab22833381835").getMovies();
-  MovieDb movie = movies.getMovie(id, "en", MovieMethod.credits, MovieMethod.images, MovieMethod.videos);
-  return movie.getVideos().get(0).getKey();
- }
-
- @GetMapping("/")
- // @ResponseBody
- public String getMovie(Model model) {
-  TmdbMovies movies = new TmdbApi("d84f9365179dc98dc69ab22833381835").getMovies();
-  MovieDb movie = movies.getMovie(550, "en", MovieMethod.credits, MovieMethod.images, MovieMethod.videos);
-  System.out.println(movie.getVideos().get(0).getKey());
-  // System.out.println(movie.getReleaseDate());
-  // System.out.println(movie.getCredits().getCast());
-  // System.out.println(movies.getPopularMovies("en", 1));
-  List<MovieDb> top20 = movies.getPopularMovies("en", 1).getResults();
-  List<List<MovieDb>> nested = new ArrayList<>();
-  // System.out.println("---------------HERE-----------------");
-  // System.out.println(top20.size());
-
-  for (int i = 0; i < 20; i += 5) { // 1
-   List<MovieDb> list = new ArrayList<>();
-   for (int j = i; j < i + 5; j++) { // 3
-    list.add(top20.get(j));
-   }
-   nested.add(list);
+  private Long getUserId() {
+    SecurityContext context = SecurityContextHolder.getContext();
+    Authentication authentication = context.getAuthentication();
+    Long id = userRepository.findByUsername(authentication.getName()).getId();
+    return id;
   }
 
-  List<MovieDb> firstList = new ArrayList<>(nested.get(0));
-  nested.remove(0);
+  private String getMovieVideoLink(int id) {
+    TmdbMovies movies = new TmdbApi("d84f9365179dc98dc69ab22833381835").getMovies();
+    MovieDb movie = movies.getMovie(id, "en", MovieMethod.credits, MovieMethod.images, MovieMethod.videos);
+    return movie.getVideos().get(0).getKey();
+  }
 
-  model.addAttribute("firstList", firstList);
-  model.addAttribute("movies", nested);
-  model.addAttribute("watch", movies);
+  @GetMapping("/")
+  // @ResponseBody
+  public String getMovie(Model model) {
+    TmdbMovies movies = new TmdbApi("d84f9365179dc98dc69ab22833381835").getMovies();
+    MovieDb movie = movies.getMovie(286217, "en", MovieMethod.credits, MovieMethod.images, MovieMethod.videos);
+    System.out.println(movie.getVideos().get(0).getKey());
+    List<MovieDb> top20 = movies.getPopularMovies("en", 1).getResults();
+    List<Integer> top20id = top20.stream().map(x -> x.getId()).collect(Collectors.toList());
 
-  // System.out.println(firstList);
-  // System.out.println(secondList);
-  System.out.println(nested);
-  return "movies/homepage";
- }
+    System.out.println(top20id);
 
- @GetMapping("/test")
- @ResponseBody
- public MovieDb getData() {
-  TmdbMovies movies = new TmdbApi("d84f9365179dc98dc69ab22833381835").getMovies();
-  MovieDb movie = movies.getMovie(550, "en", MovieMethod.credits, MovieMethod.images, MovieMethod.videos);
-  return movie;
- }
+    List<MovieDb> top20Vid = new ArrayList<>();
+    for (int i = 0; i < top20id.size(); i++) {
+      top20Vid = top20id.stream()
+          .map(x -> movies.getMovie(x, "en", MovieMethod.images, MovieMethod.videos))
+          .collect(Collectors.toList());
+    }
+
+    List<List<MovieDb>> nested = new ArrayList<>();
+    List<List<String>> videoNested = new ArrayList<>();
+
+    for (int i = 0; i < 20; i += 5) { // 1
+      List<MovieDb> list = new ArrayList<>();
+      List<String> vidList = new ArrayList<>();
+      for (int j = i; j < i + 5; j++) { // 3
+        list.add(top20Vid.get(j));
+        if (top20Vid.get(j).getVideos() == null || top20Vid.get(j).getVideos().size() == 0) {
+          vidList.add("");
+        } else {
+          vidList.add(top20Vid.get(j).getVideos().get(0).getKey());
+        }
+        videoNested.add(vidList);
+        nested.add(list);
+      }
+    }
+
+    System.out.println(videoNested);
+    List<MovieDb> firstList = new ArrayList<>(nested.get(0));
+    nested.remove(0);
+    videoNested.remove(0);
+
+    model.addAttribute("firstList", firstList);
+    // return firstList.get(0);
+    model.addAttribute("vidExt", videoNested);
+    model.addAttribute("movies", nested);
+    System.out.println(nested.get(0).get(0).getVideos().get(0).getKey());
+    model.addAttribute("watch", movies);
+
+    // System.out.println(firstList);
+    // System.out.println(secondList);
+    // System.out.println(nested);
+    return "movies/homepage";
+  }
+
+  @GetMapping("/test")
+  @ResponseBody
+  public MovieDb getData() {
+    TmdbMovies movies = new TmdbApi("d84f9365179dc98dc69ab22833381835").getMovies();
+    MovieDb movie = movies.getMovie(550, "en", MovieMethod.credits, MovieMethod.images, MovieMethod.videos);
+    return movie;
+  }
 }
