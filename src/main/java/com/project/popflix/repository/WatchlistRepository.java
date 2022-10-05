36b6fd8b9f7881b3d553f75b076c07f9aaa727ec
srodrichu <sebastianawrchu@gmail.com>
2022-10-05 17:32:05 +0100

    watchlist controller progress

--- src/main/java/com/project/popflix/repository/WatchlistRepository.java
+++ src/main/java/com/project/popflix/repository/WatchlistRepository.java
@@ -1,8 +1,12 @@
 package com.project.popflix.repository;
 
+
 import org.springframework.data.repository.CrudRepository;
 
-import com.project.popflix.model.Authority;
+import com.project.popflix.model.Watchlist;
 
-public interface WatchlistRepository extends CrudRepository<Authority, Long> {
+public interface WatchlistRepository extends CrudRepository<Watchlist, Long> {
+
+    Iterable<Watchlist> findByUserid(Long userid);
+
 }
