439eb15a45944db3b0c866997d1d4768037448b8
hakareem <harithakareem72@gmail.com>
2022-10-12 14:22:35 +0100

    watchlist repo query

--- src/main/java/com/project/popflix/repository/WatchlistRepository.java
+++ src/main/java/com/project/popflix/repository/WatchlistRepository.java
@@ -1,5 +1,8 @@
 package com.project.popflix.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.project.popflix.model.Watchlist;
@@ -11,4 +14,7 @@ public interface WatchlistRepository extends CrudRepository<Watchlist, Long> {
     Watchlist findByUseridAndMovieid(Long userid, Integer movieid);
 
     void deleteById(Long id);
+
+    @Query(value = "SELECT * FROM watchlist WHERE userid = ?1", nativeQuery = true)
+    Iterable<Watchlist> findAllMoviesByUserid(Long userid, Integer movieid); // needs to check for watchlist by userid
 }
